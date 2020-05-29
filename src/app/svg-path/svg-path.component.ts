0b41dcfb0bfcb848fbea4501ce8246740b32bcf9
Joji <noypako@hotmail.com>
2020-05-28 20:02:13 -0700

    Added random generated coordinates

--- src/app/svg-path/svg-path.component.ts
+++ src/app/svg-path/svg-path.component.ts
@@ -11,18 +11,37 @@ export class SvgPathComponent implements OnInit {
     jsonCoords: any;
     pathPts: string;
     coordPts: number[];
+    numCoords: number;
 
     constructor() {
         this.jsonCoords = new JsonReaderModule();
         this.pathPts = '';
         this.coordPts = this.jsonCoords.getCoords();
+        this.numCoords = 15;
     }
 
-    drawSVGPath() {
+    drawSVGPathJSON() {
         this.pathPts = '';
         for (let i in this.coordPts) {
             this.pathPts += `${this.coordPts[i][0]},${this.coordPts[i][1]} `;
         }
+    }
+
+    drawSVGPathRand() {
+        this.pathPts = '';
+        let intervalHandler;
+        let i = 0;
+
+        intervalHandler = setInterval(() => {
+            if (i < this.numCoords) {
+                let xPos = Math.floor(Math.random() * 700) + 10;
+                let yPos = Math.floor(Math.random() * 730) + 10;
+                this.pathPts += `${xPos},${yPos} `;
+                i++;
+            } else {
+                clearInterval(intervalHandler);
+            }
+        }, 500);
     }
 
     ngOnInit() {
